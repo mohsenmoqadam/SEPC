4600a59d5c4cfbb9274fb8695f3eb3c539178d1e
Mohsen <mohsenmoqadam@yahoo.com>
2018-01-04 12:51:39 +0330

    correcting stage tarball path

--- sepc.sh
+++ sepc.sh
@@ -361,7 +361,7 @@ rel-stage:
 	\$(PWD)/script/rebar3 as stage tar
 	\$(SED) -i 's/{$AppName, "\$(VER)"}/{$AppName, "$AppName-version"}/g' ./rebar.config
     #\$(SCP) -P 8522 \$(PWD)/_build/stage/rel/$AppName/$AppName-\$(VER).tar.gz \$(FS)
-${TAB}@printf "\nApplication: %s\n" \$(PWD)/_build/prod/rel/$AppName/$AppName-\$(VER).tar.gz
+${TAB}@printf "\nApplication: %s\n" \$(PWD)/_build/stage/rel/$AppName/$AppName-\$(VER).tar.gz
 
 rel-dev:
 	\$(PWD)/script/rebar3 as dev release
